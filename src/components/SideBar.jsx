e3c09a58a896b65bb7fbf4ce13f0956811aa099b
Vamsibuddaraju <vamshikrishnamraju007@gmail.com>
2025-04-02 10:09:20 +0530

    made changes to font and also added few more filters in customfilters

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -8,13 +8,13 @@ const SideBar = () => {
   );
   if (!sideBarView) return null;
   return (
-    <div className="flex -mt-10 pr-2 h-screen hidden sm:block flex-col pt-8 shadow-2xl w-[26%] md:w-[16%]">
-      {sideBarList.map((item) => {
+    <div className="flex -mt-10 pr-2 h-screen sm:block flex-col pt-8 shadow-2xl w-[26%] md:w-[16%]">
+      {sideBarList.map((item,index) => {
         return (
-          <Link to={`/${item.list.toLowerCase()}`} key={item.list}>
+          <Link to={index!==0?`/${item.list.toLowerCase()}`:"/"} key={item.list}>
             <div className="flex pl-6 h-12">
               {/* <img className='w-20 h-16' src={item.url} alt={item.name}/> */}
-              <p>{item.list}</p>
+              <p className="font-bold text-[18px]">{item.list}</p>
             </div>
           </Link>
         );
